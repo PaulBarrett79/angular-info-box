5ea78b4eccb623aeae53dcc31a0d48ccf3a9db0f
paulbarrett79 <paulbarrett79@gmail.com>
2015-06-12 16:24:11 +0100

    v0.0.1

--- infobox.js
+++ infobox.js
@@ -8,40 +8,40 @@ angular.module('angular-info-box', [])
         element.text('this is the infoBox directive');
       }*/
       controller: ['$scope', function($scope) {
-      	//$scope.tooltext = "test here";
-      	
+        //$scope.tooltext = "test here";
+        
 
         $('[data-toggle="popover"]').popover( {trigger: 'focus'} );
         //always default to freetext if both it and lookup are present
-      	if($scope.freetext) {
-      		$scope.displaytext = $scope.freetext;
-      	}
-      	else {
+        if($scope.freetext) {
+          $scope.displaytext = $scope.freetext;
+        }
+        else {
           //check that lookup items have been supplied
-          if(!$scope.lookupItems) {
+          if(!$scope.lookupitems) {
             //we can't look anything up - return empty string
-            console.log("InfoBox Error - lookups object is empty, supply some or use freetext attribute");
+            console.log("InfoBox Error for " + $scope.lookup + " - lookups object is empty, supply some or use freetext attribute");
             $scope.displaytext = "";
           }
           //if it's not null, we need to check it is an object (not str, array etc)
-          else if(angular.isObject($scope.lookupItems)){
+          else if(!angular.isObject($scope.lookupitems)){
 
           }
           else {
-            $scope.displaytext = lookupItems[lookup];
+            $scope.displaytext = $scope.lookupitems[$scope.lookup];
           }
-      	}
+        }
 
         
-      	
+        
       }],
       scope: {
-      	//use lookup if you are using the MessageFactory service
+        //use lookup if you are using the MessageFactory service
         lookup:'@',
         //use direct if you want to enter ad hoc text
         freetext:'@',
         //if you are using a set of things to lookup with, include them here. should probably be a scoped object,
-        lookupItems: '@',
+        lookupitems: '=',
         left:'@',
 
       },
